2758421605945873f3221a878a271a3ddd819fde
alberto <albertomcastro4@gmail.com>
2025-02-26 23:47:52 +0100

    optimizacion
    Se cambia la matriz del método de `np.ndarray` a `sparse.csr_matrix`

--- edo_contorno_1.py
+++ edo_contorno_1.py
@@ -4,24 +4,22 @@ u(0) = u(3) = 0.
 """
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import linalg, integrate, sparse
+from scipy import integrate, sparse
+from scipy.sparse import linalg
 import colorsys
 from typing import Callable
 import time
 
 f: Callable[[np.floating], float | np.floating] = lambda x : 1 - x
-sol: Callable[[np.floating], np.floating] = lambda x : integrate.quad(lambda t: f(t)*(x-t), 0, x)[0] - (x / 3.0) * integrate.quad(lambda t : f(t)*(3-t), 0, 3)[0] #x*(x-3)**2 / 6.0
+sol: Callable[[np.floating], np.floating] = lambda x : integrate.quad(lambda t: f(t)*(x-t), 0, x)[0] - (x / 3.0) * integrate.quad(lambda t : f(t)*(3-t), 0, 3)[0]
 
-def method_matrix(n: int) -> np.ndarray:
-    start = time.process_time()
-    A = np.zeros((n, n))
-    np.fill_diagonal(A, -2)
-    np.fill_diagonal(A[1:], 1)
-    np.fill_diagonal(A[:, 1:], 1)
-    cputime = time.process_time() - start
+def method_matrix(n: int) -> sparse.csr_matrix:
+    diagonals = [-2 * np.ones(n), np.ones(n - 1), np.ones(n - 1)]
+    offsets = [0, -1, 1]
+    A = sparse.diags(diagonals, offsets, format='csr')
     return A
 
-def method(n: int, f:Callable[[np.floating], float | np.floating], a:float, b:float) -> list[np.ndarray]:
+def method(n: int, f: Callable[[np.floating], float | np.floating], a: float, b: float) -> list[np.ndarray]:
     x = np.linspace(a, b, n + 2)
     xx = x[1:-1]
 
@@ -30,7 +28,7 @@ def method(n: int, f:Callable[[np.floating], float | np.floating], a:float, b:fl
 
     L = (1/h)**2 * method_matrix(n)
     
-    fn = linalg.solve(L, y)
+    fn = linalg.spsolve(L, y)
     fn = np.insert(fn, 0, 0)
     fn = np.insert(fn, n + 1, 0)
 
@@ -43,7 +41,7 @@ plt.rcParams.update({
     "font.size": 12
 })
 
-fig, axes = plt.subplot_mosaic([['solution', 'solution'], ['error,', 'time']])
+fig, axes = plt.subplot_mosaic([['solution', 'solution'], ['solution', 'solution'], ['error,', 'time']])
 fig.suptitle(r" Aproximación de la solución de $u''(x) = 1 - x, x \in (0, 3)$, $u(0) = u(3) = 0$")
 ax1 = fig.axes[0]
 ax1.title.set_text(r'Aproximación $u_n$ vs. solución $u(x) = \frac{1}{6}x(x-3)^2$')
@@ -64,24 +62,24 @@ ax3.set_ylabel(r'$t (s)$')
 for ax in axes.values():
     ax.axhline(0, color='black', lw=0.5)
     ax.axvline(0, color='black', lw=0.5)
+    ax.grid(True, which='both', linestyle='--', lw=0.5)
+
 
 left_v  = 0
 right_v = 3
 
-iters = [5, 10, 20, 50, 100, 500, 1000]
+iters = [5, 10, 20, 50, 100, 500, 1000, 4000, 8000, 12000]
 for i, n in enumerate(iters):
-    start = time.process_time()
+    start = time.perf_counter()
     x, y = method(n, f, left_v, right_v)
-    cputime = time.process_time() - start
+    cputime = time.perf_counter() - start
 
     color1 = colorsys.hsv_to_rgb(35 / 360.0, ((i + 1) / (len(iters)+1)) ** (0.5) , 1)
     ax1.plot(x, y, label=rf'$u_{{{n}}}$', color = color1)
 
     error = np.max(np.abs(y - [sol(i) for i in x]))
-    ax2.plot(1.0 / n, error, 'o-', color = 'blue')
-    ax3.plot(n, cputime, 'o-', color = 'red')
-
-
+    ax2.plot(1.0 / n, error, 'x-', color = 'blue')
+    ax3.plot(n, cputime, 'x-', color = 'red')
 
 x = np.linspace(left_v, right_v, 100)
 y = [sol(i) for i in x]
@@ -89,7 +87,6 @@ color1 = colorsys.hsv_to_rgb(35 / 360.0, 1 , 1)
 ax1.plot(x, y, label='solución', color = color1)
 
 ax1.legend()
-ax2.legend()
 plt.show()
 
 
