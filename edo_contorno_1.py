3181138ed3ec8a51c00c43c2fdde6f8be2348ed3
Alberto Moreno Castro <albertomcastro4@gmail.com>
2025-02-26 12:59:03 +0100

    update

--- edo_contorno_1.py
+++ edo_contorno_1.py
@@ -1,9 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import linalg, integrate
+from scipy import linalg, integrate, sparse
 import colorsys
 from typing import Callable
-import time, math
+import time
 
 def method_matrix(n: int) -> np.ndarray:
     start = time.process_time()
@@ -15,9 +15,8 @@ def method_matrix(n: int) -> np.ndarray:
     print(f"Took {cputime} seconds to create matrix of order {n}")
     return A
 
-def method(n: int):
-    x = np.linspace(0, 1, n + 2)
-    #erase first and last element
+def method(n: int, f:Callable[[np.floating], float | np.floating], a:float, b:float) -> list[np.ndarray]:
+    x = np.linspace(a, b, n + 2)
     xx = x[1:-1]
 
     y = [f(i) for i in xx]
@@ -29,27 +28,45 @@ def method(n: int):
     fn = np.insert(fn, 0, 0)
     fn = np.insert(fn, n + 1, 0)
 
-    color1 = colorsys.hsv_to_rgb(35 / 360.0, 0.3 , 1)#@0.3 + 0.7 * (1 - (1.0 * diff1 / first_diff1)), 1)
-    plt.plot(x, fn, label=f'n={n}', color = color1)
+    return [x, fn]
+    #color1 = colorsys.hsv_to_rgb(35 / 360.0, 0.3 , 1)#@0.3 + 0.7 * (1 - (1.0 * diff1 / first_diff1)), 1)
+    #plt.plot(x, fn, label=f'n={n}', color = color1)
 
-f: Callable[[np.floating], float | np.floating] = lambda x : x
-sol: Callable[[np.floating], np.floating] = lambda x : integrate.quad(lambda t: f(t)*(x-t), 0, x)[0] - x*integrate.quad(lambda t : f(t)*(1-t), 0, 1)[0]
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
-plt.axhline(0, color='black', lw=0.5)
-plt.axvline(0, color='black', lw=0.5)
+f: Callable[[np.floating], float | np.floating] = lambda x : x ** 7
+sol: Callable[[np.floating], np.floating] = lambda x : integrate.quad(lambda t: f(t)*(x-t), 0, x)[0] - x*integrate.quad(lambda t : f(t)*(3-t), 0, 3)[0] #x*(x-3)**2 / 6.0
 
-start = time.process_time()
-method(10000)
-cputime = time.process_time() - start
-print(f"Took {cputime} seconds to solve the problem")
+# draw axes
+ax1.axhline(0, color='black', lw=0.5)
+ax1.axvline(0, color='black', lw=0.5)
+ax2.axhline(0, color='black', lw=0.5)
+ax2.axvline(0, color='black', lw=0.5)
 
 
-x = np.linspace(0, 1, 100)
+left_v  = 0
+right_v = 3
+
+for i, n in enumerate([5, 10, 20, 50, 100, 500, 1000]):
+    start = time.process_time()
+    x, y = method(n, f, left_v, right_v)
+    cputime = time.process_time() - start
+    print(f"Took {cputime} seconds to solve the problem")
+
+    color1 = colorsys.hsv_to_rgb(35 / 360.0, ((i + 1) /9.0) ** (0.5) , 1)
+    ax1.plot(x, y, label=f'n={n}', color = color1)
+
+    error = np.max(np.abs(y - [sol(i) for i in x]))
+    ax2.plot(n, error, 'o', color = 'blue')
+
+
+x = np.linspace(left_v, right_v, 100)
 y = [sol(i) for i in x]
 color1 = colorsys.hsv_to_rgb(35 / 360.0, 1, 1)
-plt.plot(x, y, label='solution', color = color1)
+ax1.plot(x, y, label='solution', color = color1)
 
-plt.legend()
+ax1.legend()
+ax2.legend()
 plt.show()
 
 
