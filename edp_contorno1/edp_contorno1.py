9e55b4d282c7661ab4b1e0d8f098f3c3280c5b09
alberto <albertomcastro4@gmail.com>
2025-03-26 11:49:20 +0100

    Compra pasta de dientes

--- edp_contorno1/edp_contorno1.py
+++ edp_contorno1/edp_contorno1.py
@@ -4,16 +4,25 @@ u(0) = u(3) = 0.
 """
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
 from scipy import integrate, sparse
 from scipy.sparse import linalg
 import colorsys
 from typing import Callable
 import time
 
+function_t  = Callable[[float | np.floating], float | np.floating]
+function2_t = Callable[[float | np.floating, float | np.floating], float | np.floating]
+constant_t    = float | np.floating
+class square_boundary_condition:
+    def __init__(self, left: function2_t | constant_t = 0, right: function2_t | constant_t = 0, bottom: function2_t | constant_t = 0, top: function2_t | constant_t = 0) -> None:
+        self.left = left if callable(left) else lambda x, y : left
+        self.right = right if callable(right) else lambda x, y : right
+        self.bottom = bottom if callable(bottom) else lambda x, y : bottom
+        self.top = top if callable(top) else lambda x, y : top
+
 # Definición de las funciones f y solución.
-f: Callable[[np.floating, np.floating], float | np.floating] = lambda x, y : np.cos(3*x) * np.sin(3 * y)
-#sol: Callable[[np.floating], np.floating] = lambda x : integrate.quad(lambda t: f(t)*(x-t), 0, x)[0] - (x / 3.0) * integrate.quad(lambda t : f(t)*(3-t), 0, 3)[0]
-sol: Callable[[np.floating, np.floating], np.floating] = lambda x : x**2 / 2 - x**3 / 6
+f: function2_t = lambda x, y : 1 - np.sqrt(x**2 + y**2)
 
 def matrix_to_vector_index(i: int, j: int, n: int) -> int:
     return i + j * n
@@ -31,7 +40,10 @@ def method_matrix(n: int) -> sparse.csr_matrix:
     return A
 
 # Método de diferencias finitas para resolver el problema de contorno.
-def method(n: int, f: Callable[[np.floating], float | np.floating], a: float, b: float) -> list[np.ndarray]:
+def method(n: int, f: function2_t, a: constant_t, b: constant_t, boundary_condition: square_boundary_condition = square_boundary_condition()) -> list[np.ndarray]:
+    if b < a:
+        swap = a; a = b; b = swap
+
     x = np.linspace(a, b, n + 1)
     y = np.linspace(a, b, n + 1)
     xx = x[1:-1]
@@ -41,16 +53,19 @@ def method(n: int, f: Callable[[np.floating], float | np.floating], a: float, b:
     h = xx[1] - xx[0]
 
     L = (1 / h)**2 * method_matrix(n)
-    
-    fn = linalg.spsolve(L, z)
+    boundary_values = {
+        "left" : [0.5 * (boundary_condition.left(a, b) + boundary_condition.top(a,b))] + [boundary_condition.left(a, l) for l in yy] + [0.5 * (boundary_condition.left(a, b) + boundary_condition.bottom(a,b))],
+        "right" : [0.5 * (boundary_condition.right(a, b) + boundary_condition.top(b,b))] + [boundary_condition.right(b, l) for l in yy] + [0.5 * (boundary_condition.right(a, b) + boundary_condition.bottom(b,b))],
+        "bottom" : [0.5 * (boundary_condition.bottom(a, b) + boundary_condition.left(a,a))] + [boundary_condition.bottom(l, a) for l in xx] + [0.5 * (boundary_condition.bottom(a, b) + boundary_condition.right(b,a))],
+        "top" : [0.5 * (boundary_condition.top(a, b) + boundary_condition.left(b,b))] + [boundary_condition.top(l, b) for l in xx] + [0.5 * (boundary_condition.top(a, b) + boundary_condition.right(b,b))]
+    }
+    boundary_vector = sparse.csr_matrix(np.concatenate([boundary_values["bottom"], np.zeros((n-1)**2), boundary_values["top"]]))
+    zz = linalg.spsolve(L, z + sparse.flatten(boundary_vector).tolist())
 
-    zz = np.array(fn).reshape(n-1, n-1)
-    zz = np.concatenate((np.zeros((n-1, 1)), zz, np.zeros((n-1, 1))), axis=1)
-    zz = np.concatenate((np.zeros((1, n+1)), zz, np.zeros((1, n+1))), axis=0)
-
+    zz = np.array(zz).reshape(n-1, n-1)
+    zz = np.concatenate((np.reshape(boundary_values["bottom"][1:-1], (1, n - 1)), zz, np.reshape(boundary_values["top"][1:-1], (1, n - 1))), axis=0)
+    zz = np.concatenate((np.reshape(boundary_values["left"], (n + 1, 1)), zz, np.reshape(boundary_values["right"], (n + 1, 1)),), axis=1)
     mesh_xx, mesh_yy = np.meshgrid(x, y)
-    print(zz)
-
     return [mesh_xx, mesh_yy, zz]
 
 plt.rcParams.update({
@@ -60,38 +75,38 @@ plt.rcParams.update({
     "font.size": 12
 })
 
-fig = plt.figure()
-ax1 = plt.axes(projection='3d')
-fig.suptitle(r" Aproximación de la solución de $u''(x) = 1 - x, x \in (0, 3)$, $u(0) = u(3) = 0$")
+fig, axes = plt.subplot_mosaic([['main', 'contour']], per_subplot_kw= {
+    'main': {"projection" : "3d"},
+    'contour': {},
+})
+
+fig.suptitle(r" Aproximación de la solución de $\Delta u(x) = f, x \in ]0, 1)[\times]0,1[$, $\left.u\right|_{\partial\Omega} = 0$")
 #Make 'projection=3d' to ax1
-ax1.title.set_text(r'Aproximación $u_N$ vs. solución $u(x) = \frac{x^2}{2}-\frac{x^3}{6}$')
-ax1.set_xlabel(r'$x$')
-ax1.set_ylabel(r'$u(x)$')
+#ax1.title.set_text(r'Aproximación $u_N$ vs. solución $u(x) = \frac{x^2}{2}-\frac{x^3}{6}$')
+#ax1.set_xlabel(r'$x$')
+#ax1.set_ylabel(r'$u(x)$')
+
+ax1, ax2 = axes['main'], axes['contour']
 
 method_matrix(4)
 
 left_v  = 0
 right_v = 1
 
-iters = [30]
+iters = [100]
 cputime, error, cputime_old, error_old, n_old = 0, 0, 0, 0, -1
+
+boundary_condition = square_boundary_condition(1, 1, 1, 1)
+
 for i, n in enumerate(iters):
     start = time.perf_counter()
-    x, y, z = method(n, f, left_v, right_v)
+    x, y, z = method(n, f, left_v, right_v, boundary_condition)
     cputime_old = cputime
     cputime = time.perf_counter() - start
 
     color1 = colorsys.hsv_to_rgb(35 / 360.0, ((i + 1) / (len(iters)+1)) ** (0.5) , 1)
-    #create a mesh grid
-    ax1.plot(x, y, z, label=f'$u_{n}$', color=color1, alpha=0.5)
-
-    #error_old = error
-    #error = np.max(np.abs(y - [sol(i) for i in x]))
-
-    #if i > 0:
-    #    ax2.plot([1/n_old, 1/n], [error_old, error], 'o', color = 'red', linestyle='-')
-    #    ax3.plot([n_old, n], [cputime_old, cputime], 'o', color = 'blue', linestyle='-')
-    n_old = n
+    ax1.plot_surface(x, y, z, label=f'$u_{n}$', cmap = cm.viridis, alpha=0.7)
+    ax2.contour(x, y, z, cmap = cm.viridis, alpha=0.7)
 
 #if len(x) < 100:
 #    x = np.linspace(left_v, right_v, 100)
--- euler_calor/calor_euler.py
+++ euler_calor/calor_euler.py
@@ -4,14 +4,21 @@ u(0) = u(3) = 0.
 """
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib import animation as anim
 from scipy import integrate, sparse
 from scipy.sparse import linalg
 import colorsys
 from typing import Callable
 import time
 
+
+constant_t    = float | np.floating
+function_t  = Callable[[constant_t], constant_t]
+function2_t = Callable[[constant_t, constant_t], constant_t]
+
 # Definición de las funciones u_0.
-u_0: Callable[[np.floating], float | np.floating] = lambda x : 1
+u_0: function_t = lambda x : -(x-1)*np.sin(2*np.pi*x)
 
 # Obtención de la matriz (sparse.csr_matrix) para n arbitrario.
 def method_matrix(n: int) -> sparse.csr_matrix:
@@ -21,27 +28,25 @@ def method_matrix(n: int) -> sparse.csr_matrix:
     return A
 
 # Método de diferencias finitas para resolver el problema de contorno.
-def method(n: int, m: int, init_cond: Callable[[np.floating], float | np.floating], a: float = 0, b: float = 1, t_0: float = 0, t_1: float = 1) -> list[np.ndarray]:
+def method(n: int, m: int, init_cond: function_t, a: float = 0, b: float = 1, t_0: float = 0, t_1: float = 1) -> list[np.ndarray]:
     x = np.linspace(a, b, n + 1)
     xx = x[1:-1]
     t = np.linspace(t_0, t_1, m + 1)
-    tt = t[1:-1]
+    tt = t[:-1]
 
     u0 = [init_cond(i) for i in xx]
     h = x[1] - x[0]
     k = 1 / m
 
-    L = method_matrix(n - 1)
+    L = (k / h**2) * method_matrix(n - 1)
     u = u0
-    uu = []
-    print(np.shape(L), np.shape(u))
+    uu = [np.concatenate(([0], u0, [0]))]
     for _ in range(1, m):
-        print(f"{_} : {((k / h**2) * L) @ u}")
-        u = u + ((k / h**2) * L) @ u
-        uu.append([0] + u + [0])
-    
-    xxx, ttt = np.meshgrid(x, t)
-    print(np.shape(xxx), np.shape(ttt), np.shape(uu))
+        u = u +  L @ u
+        uu.append(np.concatenate(([0], u, [0])))
+
+    xxx, ttt = np.meshgrid(x, tt)
+
     return [xxx, ttt, np.array(uu)]
 
 plt.rcParams.update({
@@ -53,42 +58,27 @@ plt.rcParams.update({
 
 fig = plt.figure()
 ax1 = plt.axes(projection='3d')
-fig.suptitle(r" Aproximación de la solución de $u''(x) = 1 - x, x \in (0, 3)$, $u(0) = u(3) = 0$")
-#Make 'projection=3d' to ax1
-ax1.title.set_text(r'Aproximación $u_N$ vs. solución $u(x) = \frac{x^2}{2}-\frac{x^3}{6}$')
 ax1.set_xlabel(r'$x$')
 ax1.set_ylabel(r'$t$')
-
-method_matrix(4)
+ax1.set_zlabel(r'$u(x, t)$')
+ax1.set_title(r'Solución de la ecuación del calor con condición inicial $u(x, 0) = u_0$')
 
 left_v  = 0
-right_v = 1
+right_v = 2
 
-iters = [(10, 5000)]
+iters = [(50, 5000)]
 cputime, error, cputime_old, error_old, n_old = 0, 0, 0, 0, -1
 for i, (n, m) in enumerate(iters):
-    start = time.perf_counter()
-    x, y, z = method(n, m, u_0)
-    cputime_old = cputime
-    cputime = time.perf_counter() - start
+    x, y, z = method(n, m, u_0, left_v, right_v, 0, 1)
 
-    color1 = colorsys.hsv_to_rgb(35 / 360.0, ((i + 1) / (len(iters)+1)) ** (0.5) , 1)
-    #create a mesh grid
-    ax1.plot_surface(x, y, z, label=f'$u_{n}$', color=color1, alpha=0.5)
+    
+    for j in range(0, m, 100):
+        color1 = colorsys.hsv_to_rgb(35 / 360.0, 1.0, 1.0)
+        ax1.plot(x[j], y[j], z[j], color=color1, linewidth=3)
+        #plt.pause(0.1)
+    
+    ax1.plot_surface(x, y, z, cmap=cm.plasma, alpha=0.5)
 
-    #error_old = error
-    #error = np.max(np.abs(y - [sol(i) for i in x]))
-
-    #if i > 0:
-    #    ax2.plot([1/n_old, 1/n], [error_old, error], 'o', color = 'red', linestyle='-')
-    #    ax3.plot([n_old, n], [cputime_old, cputime], 'o', color = 'blue', linestyle='-')
-    n_old = n
-
-#if len(x) < 100:
-#    x = np.linspace(left_v, right_v, 10def method_matrix(n: int) -> sparse.csr_matrix: diagonals = [-2 * np.ones(n), np.ones(n - 1), np.ones(n - 1)] offsets = [0, -1, 1] A = sparse.diags(diagonals, offsets, format='csr') return A0)
-#y = [sol(i) for i in x]
-#color1 = colorsys.hsv_to_rgb(35 / 360.0, 1 , 1)
-#ax1.plot(x, y, label='solución', color = color1)
 
 ax1.legend()
 plt.show()
